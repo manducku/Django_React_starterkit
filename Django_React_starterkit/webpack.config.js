05bf4c329ce40c313cfca4f5029abfb4b5bf0179
manducku <alwaysfun2183@gmail.com>
2016-05-14 15:31:23 +0900

    add config of react-hot-loader

--- Django_React_starterkit/webpack.config.js
+++ Django_React_starterkit/webpack.config.js
@@ -5,20 +5,26 @@ var BundleTracker = require('webpack-bundle-tracker')
 module.exports = {  
         context: __dirname,
 
-      entry: './assets/js/index', // webpack의 시작 포인트. Root Component 경로를 입력
-
+        entry: [ 
+        './assets/js/index', // webpack의 시작 포인트. Root Component 경로를 입력
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server'
+        ],
       output: {
           path: path.resolve('./assets/bundles/'),
           filename: "[name]-[hash].js",
+          publicPath: 'http://localhost:3000/assets/bundles/',
       }, 
 
       plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(), // error가 있을 시, reload를 안하는 플러그인 
         new BundleTracker({filename: './webpack-stats.json'}),
       ],
 
       module: {
         loaders: [
-          { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel?presets[]=es2015&presets[]=react'}, 
+          { test: /\.jsx?$/, exclude: /node_modules/, loaders:['react-hot', 'babel?presets[]=es2015&presets[]=react']}, 
           // 모든 jsx 파일을 js 파일로 babel을 통해 컴파일합니다
         ],
       },
